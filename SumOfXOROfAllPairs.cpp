1d8d3a61f59afd8e53b1fec9027487613760a926
Kushagra Pandey <56930383+opkpEh@users.noreply.github.com>
2023-10-30 23:49:34 +0530

    Update SumOfXOROfAllPairs.cpp

--- SumOfXOROfAllPairs.cpp
+++ SumOfXOROfAllPairs.cpp
@@ -11,12 +11,57 @@ public:
                     countSetBits++;
                 }
             }
-
-            // For each bit position, calculate the XOR contribution
-            // and add it to the answer
             ans += (1LL << i) * countSetBits * (n - countSetBits);
         }
 
         return ans;
     }
 };
+
+// unrefined code:
+
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    // Returns sum of bitwise OR
+    // of all pairs
+    long long int sumXOR(int arr[], int n)
+    {
+        int ans = 0;
+        std::vector <int> arrBin;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (j > i)
+                {
+                    arrBin.push_back(arr[i] ^ arr[j]);
+                }
+            }
+        }
+
+
+        for (auto ele : arrBin)
+        {
+            ans += ele;
+        }
+        return ans;
+
+    }
+};
+
+    int main()
+    {
+        Solution sol;
+
+        int input[] = { 3, 5, 7 };
+
+        long long int answer = sol.sumXOR(input, 3);
+
+        std::cout << answer;
+
+        return 0;
+    }
